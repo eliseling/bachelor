10a6ee7c16322d7f4863fb902cc44759712e5da0
eliseling <eliseling2002@gmail.com>
2026-03-12 11:06:00 +0100

    Icon fix and menu bkg.

--- js/script.js
+++ js/script.js
@@ -263,3 +263,7 @@ window.addEventListener('scroll', () => {
   else topBtn.classList.remove('show');
 });
 topBtn.addEventListener('click', () => window.scrollTo({top:0, behavior:'smooth'}));
+
+document.addEventListener("DOMContentLoaded", () => {
+  lucide.createIcons();
+});
